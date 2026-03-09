2eacd11121f3c55651befa0e8ad91f214910ac23
Yang Liu <summerliu2333@gmail.com>
2026-03-09 14:13:05 -0700

    now collection use default constructor

--- lab20.cpp
+++ lab20.cpp
@@ -77,16 +77,15 @@ int main() {
     delete livingChair;
     livingChair = nullptr;
 
-    //creating dynamic array of chair objects
+    //uses default constructor
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
+
+    delete[] collection;
+    collection = nullptr;
+    delete chairPtr;
+    chairPtr = nullptr;
     
     return 0;
 }
